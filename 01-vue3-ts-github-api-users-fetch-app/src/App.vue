4c2f53f8021a7ec3d23aa02136f6985648b58aeb
danielOuattara <daniel.ouattara@gmx.com>
2023-01-31 20:14:00 +0100

    update logic & imports

--- 01-vue3-ts-github-api-users-fetch-app/src/App.vue
+++ 01-vue3-ts-github-api-users-fetch-app/src/App.vue
@@ -1,28 +1,52 @@
 <template>
-  <SingleUSer />
+  <SingleUser
+    @startFetchUser="startFetchUser"
+    :singleUserData="singleUserData"
+    :singleUserLoading="singleUserLoading"
+    :singleUserError="singleUserError"
+    :singleUserErrorMessage="singleUserErrorMessage"
+  />
   <UsersList v-if="!isLoading && !isError" :users="users" />
 </template>
 
 <script lang="ts">
 import { defineComponent, onBeforeMount } from "vue";
 import UsersList from "./components/UsersList.vue";
-import SingleUSer from "./components/SingleUSer.vue";
-// import User from "../types/User";
+import SingleUser from "./components/SingleUser.vue";
 import useFetchUsers from "./composables/useFetchUsers";
+import useFetchSingleUser from "./composables/useFetchSingleUser";
 export default defineComponent({
   name: "App",
-  components: { UsersList, SingleUSer },
+  components: { UsersList, SingleUser },
   setup() {
     const { isLoading, isError, errorMessage, users, fetchUsers } =
       useFetchUsers();
 
     onBeforeMount(fetchUsers);
 
+    const {
+      singleUserLoading,
+      singleUserError,
+      singleUserErrorMessage,
+      singleUserData,
+      fetchSingleUser,
+    } = useFetchSingleUser();
+
+    function startFetchUser(arg: string) {
+      fetchSingleUser(arg);
+    }
+
     return {
       isLoading,
       isError,
       errorMessage,
       users,
+      singleUserLoading,
+      singleUserError,
+      singleUserErrorMessage,
+      singleUserData,
+      fetchSingleUser,
+      startFetchUser,
     };
   },
 });
