0dd78dc12de2119f01c02504abd00563262ee03a
danielOuattara <daniel.ouattara@gmx.com>
2023-01-31 20:57:11 +0100

    Loading + ErrorMessage comp

---
+++ 02-vue3-ts_travel-tours-fetch-app/src/components/ErrorMessage.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <div className="title">
+      <h2>{{ errorMessage }}</h2>
+    </div>
+  </main>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "ErrorMessage",
+  props: ["errorMessage"],
+});
+</script>
---
+++ 02-vue3-ts_travel-tours-fetch-app/src/components/LoadingMessage.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    <div className="loading">
+      <h1>loading...</h1>
+    </div>
+  </main>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "LoadingMessage",
+});
+</script>
