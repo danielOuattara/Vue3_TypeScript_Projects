b2d3970b8b93922c8f3b249729421647529c1c49
danielOuattara <daniel.ouattara@gmx.com>
2023-01-31 20:13:07 +0100

    composables: useFetchSinglUser

---
+++ 01-vue3-ts-github-api-users-fetch-app/src/composables/useFetchSingleUser.ts
@@ -0,0 +1,33 @@
+import User from "types/User";
+import { reactive, toRefs } from "vue";
+
+function useFetchSingleUser() {
+    const state = reactive({
+        singleUserLoading: true,
+        singleUserError: false,
+        singleUserErrorMessage: "",
+        singleUserData: <User>{},
+    });
+
+    const url = "https://api.github.com/users";
+
+    async function fetchSingleUser(usernameArg: string) {
+        try {
+            const res = await fetch(`${url}/${usernameArg}`);
+            if (!res.ok) {
+                state.singleUserError = true;
+                state.singleUserLoading = false;
+                state.singleUserErrorMessage = `${res.statusText} ${res.status}`;
+                throw Error(res.statusText);
+            } else {
+                state.singleUserData = await res.json();
+            }
+        } catch (err) {
+            console.log((err as Error).message);
+        }
+    }
+
+    return { ...toRefs(state), fetchSingleUser };
+}
+
+export default useFetchSingleUser;
