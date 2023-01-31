89d50543cb36e6e1be0a2474c65cfa2f7f61e8f1
danielOuattara <daniel.ouattara@gmx.com>
2023-01-31 20:57:25 +0100

    AllTours comp

---
+++ 02-vue3-ts_travel-tours-fetch-app/src/components/AllTours.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+    <section>
+      <div className="title">
+        <h2>ours tours</h2>
+        <div className="underline"></div>
+        <div>
+          <TourItem
+            v-for="tourItem in tours"
+            :key="tourItem.id"
+            :tourItem="tourItem"
+            @removeTour="$emit('removeTour', tourItem.id)"
+          />
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import TourItem from "./TourItem.vue";
+export default defineComponent({
+  name: "AllTours",
+  components: { TourItem },
+  props: ["tours"],
+  emits: ["removeTour"],
+});
+</script>
