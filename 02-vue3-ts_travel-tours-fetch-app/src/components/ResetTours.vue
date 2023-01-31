616b19b1b72631b2742dfa20f4a61e16dc4d6de4
danielOuattara <daniel.ouattara@gmx.com>
2023-01-31 20:56:40 +0100

    ResetTours component

---
+++ 02-vue3-ts_travel-tours-fetch-app/src/components/ResetTours.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <div className="title">
+      <h2>no tour left</h2>
+      <button className="btn" @click="fetchTours">refresh</button>
+    </div>
+  </main>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "ResetTours",
+  props: ["fetchTours"],
+});
+</script>
---
+++ 02-vue3-ts_travel-tours-fetch-app/src/components/TourItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <article class="single-tour">
+    <img :src="tourItem.image" :alt="tourItem.name" />
+    <footer>
+      <div class="tour-info">
+        <h4>{{ tourItem.name }}</h4>
+        <h4 class="tour-price">${{ tourItem.price }}</h4>
+      </div>
+      <p>
+        {{
+          readMore ? tourItem.info : tourItem.info.substring(0, 150) + " ..."
+        }}
+        <button @click="readMore = !readMore">
+          {{ readMore ? "show less" : "read more" }}
+        </button>
+      </p>
+      <button class="delete-btn" @click="$emit('removeTour')">
+        remove this tour
+      </button>
+    </footer>
+  </article>
+</template>
+
+<script>
+import { ref } from "vue";
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "TourItem",
+  props: ["tourItem"],
+  emits: ["removeTour"],
+  setup() {
+    const readMore = ref(false);
+    return { readMore };
+  },
+});
+</script>
