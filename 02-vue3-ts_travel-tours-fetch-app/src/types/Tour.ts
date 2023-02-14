0b3fdce9fea9ff942882b67c3ca53b5b87662c35
danielOuattara <daniel.ouattara@gmx.com>
2023-02-15 00:18:29 +0100

    update interface

--- 02-vue3-ts_travel-tours-fetch-app/src/types/Tour.ts
+++ 02-vue3-ts_travel-tours-fetch-app/src/types/Tour.ts
@@ -1,9 +1,9 @@
 interface Tour {
-    id: string,
-    name: string,
-    info: string,
-    image: string
-    price: number
+  id: string;
+  name: string;
+  info: string;
+  image: string;
+  price: number;
 }
 
 export default Tour;
