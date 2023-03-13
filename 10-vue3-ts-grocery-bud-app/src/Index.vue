522ac425afcdddbeef9e80b2cc823a25d6fadf75
danielOuattara <daniel.ouattara@gmx.com>
2023-03-13 13:19:49 +0100

    root files

---
+++ 10-vue3-ts-grocery-bud-app/src/Index.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- <AppOptionsAPI /> -->
+  <br />
+  <hr />
+  <br />
+  <AppCompositionAPI_V1 />
+  <br />
+  <hr />
+  <br />
+  <!-- <AppCompositionAPI_V2 /> -->
+  <br />
+  <hr />
+  <br />
+  <!-- <AppCompositionAPI_V3 /> -->
+</template>
+
+<script lang="ts">
+import AppOptionsAPI from "./_01_vue3_options_api/AppOptionsAPI.vue";
+import AppCompositionAPI_V1 from "./_02_vue3_composition_api_v1/AppCompositionAPI_V1.vue";
+import AppCompositionAPI_V2 from "./_02_vue3_composition_api_v2/AppCompositionAPI_V2.vue";
+import AppCompositionAPI_V3 from "./_02_vue3_composition_api_v3/AppCompositionAPI_V3.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Index",
+  components: {
+    AppOptionsAPI,
+    AppCompositionAPI_V1,
+    AppCompositionAPI_V2,
+    AppCompositionAPI_V3,
+  },
+});
+</script>
--- 10-vue3-ts-grocery-bud-app/src/main.ts
+++ 10-vue3-ts-grocery-bud-app/src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
-import App from "./App.vue";
+import Index from "./Index.vue";
 import "./index.css";
 
-createApp(App).mount("#app");
+createApp(Index).mount("#app");
