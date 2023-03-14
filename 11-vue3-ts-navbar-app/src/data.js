8fddc1fea66952b6d9438a36a20f21025fab3afb
danielOuattara <daniel.ouattara@gmx.com>
2023-03-14 07:41:31 +0100

    starting 11 vue3-ts navbar

---
+++ 11-vue3-ts-navbar-app/src/data.js
@@ -0,0 +1,73 @@
+export const links = [
+  {
+    id: 1,
+    url: "/",
+    text: "home",
+  },
+  {
+    id: 2,
+    url: "/about",
+    text: "about",
+  },
+  {
+    id: 3,
+    url: "/projects",
+    text: "projects",
+  },
+  {
+    id: 4,
+    url: "/contact",
+    text: "contact",
+  },
+  {
+    id: 5,
+    url: "/profile",
+    text: "profile",
+  },
+];
+
+// export const social = [
+//   {
+//     id: 1,
+//     url: "https://www.twitter.com",
+//     icon: <font-awesome-icon icon="fa-brands fa-facebook" />,
+//   },
+//   {
+//     id: 2,
+//     url: "https://www.twitter.com",
+//     icon: <font-awesome-icon icon="fa-brands fa-twitter" />,
+//   },
+//   {
+//     id: 3,
+//     url: "https://www.twitter.com",
+//     icon: <font-awesome-icon icon="fa-brands fa-linkedin" />,
+//   },
+//   {
+//     id: 4,
+//     url: "https://www.twitter.com",
+//     icon: <font-awesome-icon icon="fa-brands fa-behance" />,
+//   },
+// ];
+
+export const social = [
+  {
+    id: 1,
+    url: "https://www.twitter.com",
+    icon: "fa-brands fa-facebook",
+  },
+  {
+    id: 2,
+    url: "https://www.twitter.com",
+    icon: "fa-brands fa-twitter",
+  },
+  {
+    id: 3,
+    url: "https://www.twitter.com",
+    icon: "fa-brands fa-linkedin",
+  },
+  {
+    id: 4,
+    url: "https://www.twitter.com",
+    icon: "fa-brands fa-behance",
+  },
+];
